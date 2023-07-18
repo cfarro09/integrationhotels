600d35670fd21e17d788cd177dd15326584dcd6b
Carlos Farro <fdcarlosd1@gmail.com>
2023-07-18 17:09:27 -0500

    f

--- routes/integration.js
+++ routes/integration.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { GetHotelBeds } = require('../controllers/IntegrationController');
 const router = express.Router()
 
+
 router.get('/hotelbeds', GetHotelBeds)
 
 module.exports = router
